038d2a54206be01b8e4bf57f9bc1ffab212b8de6
Alexander Tolich <aleksandrtolich@gmail.com>
2018-12-02 23:25:35 +0300

    implement base algorithm

--- src/com/traning/task5/BaseAlg.java
+++ src/com/traning/task5/BaseAlg.java
@@ -8,34 +8,267 @@ import java.util.List;
 import java.util.Map;
 
 public class BaseAlg {
-    private TaskWrapper tw;
-    private int k1 = 2;
-    private int k2 = 2;
-    private int k3 = 2;
+    private Model tw;
+    private int k1 = 8;
+    private int k2 = 1;
+    private int k3 = 7;
     private Cluster cluster;
 
-    public BaseAlg(TaskWrapper tw) {
+    public BaseAlg(Model tw) {
         this.tw = tw;
         cluster = new Cluster(0);
     }
     
-    public int solve(){
+    public double solve(){
         clustering();
-        return -1;
+        innerSolving(cluster);
+        return getSolution(cluster);
+    }
+
+    private double getSolution(Composite composite){
+        if(composite.getSize() < 1) return -1;
+        Node node = composite.getNodes().get(0);
+        Node cur = node.nextNode();
+        double sol = TownUtils.getDist(node, cur);
+        while (cur != node){
+            Node tmp = cur.nextNode();
+            sol += TownUtils.getDist(cur, tmp);
+            cur = tmp;
+        }
+        return sol;
     }
     
     private void clustering(){
         List<Town> list = new ArrayList<>(tw.getTownList());
         if(list.size() <= 2) throw new UnsupportedOperationException();
         cluster = innerClustering(list, 0,0);
+    }
 
+    private void innerSolving(Composite cluster){
+        setOptPathCycle(cluster);
+        Cluster cluster1 = (Cluster) cluster;
+        boolean flag = true;
+        for(Node clust : cluster.getClusters()){
+            if(clust instanceof Composite){
+                innerSolving((Composite) clust);
+                flag = false;
+            }
+        }
+        if(flag) return;
+        //if all town do nothing
+        postUnion(cluster1);
+    }
 
+    private void postUnion(Cluster comp){
+        if(comp.getClusters().size() < 2) throw new UnsupportedOperationException();
+        List<Node> listClust = comp.getClusters();
+        Cluster n1 = (Cluster) listClust.get(0);
+        Cluster n2 = (Cluster) n1.nextNode();
+        Pair<Node, Node> minDistPair =  findMinNodesBClusters(n1, n2);
+        Node firstInNode = minDistPair.getKey().nextNode();
+        minDistPair.getKey().setNextNode(minDistPair.getValue());
+        Node outNode = minDistPair.getValue();
+        Cluster cl = n2;
+        for(int i = 1; i < listClust.size() - 1; i++){
+            outNode = settingPathInCluster(cl, outNode);
+            cl = (Cluster) cl.nextNode();
+        }
+        settingLastPathInCluster(cl, outNode, firstInNode);
+    }
+
+    private void settingLastPathInCluster(Cluster comp, Node maxNode, Node firstNode){
+        Node rightNode = maxNode.nextNode();
+        Node leftNode = getPrevNode(comp, maxNode);
+        double minDist = TownUtils.getDist(rightNode, firstNode);
+        Node nextOut = rightNode;
+
+        if(leftNode != null){
+            double dist = TownUtils.getDist(leftNode, firstNode);
+            if(dist < minDist){
+                nextOut = leftNode;
+            }
+        }
+
+        if(nextOut == rightNode){
+            reverseOrder(comp, maxNode, nextOut);
+            nextOut.setNextNode(firstNode);
+        } else { // nextOut == leftNode
+            nextOut.setNextNode(firstNode);
+        }
+    }
+
+    private Node settingPathInCluster(Cluster comp, Node maxNode){
+        Cluster nextClust = (Cluster) comp.nextNode();
+        Node rightNode = maxNode.nextNode();
+        Node leftNode = getPrevNode(comp, maxNode);
+        double minDist = Double.MAX_VALUE;
+        Node nextOut = null;
+        Node nextIn = null;
+        //right node
+        Pair<Node, Node> rightPair = findMinNodesBClusters(rightNode, nextClust);
+        minDist = TownUtils.getDist(rightPair.getKey(), rightPair.getValue());
+        nextOut = rightPair.getKey();
+        nextIn = rightPair.getValue();
+        //left node
+        if(leftNode != null){
+            Pair<Node, Node> leftPair = findMinNodesBClusters(leftNode, nextClust);
+            double dist = TownUtils.getDist(leftPair.getKey(), leftPair.getValue());
+            if(dist < minDist){
+                minDist = dist;
+                nextOut = leftPair.getKey();
+                nextIn = leftPair.getValue();
+            }
+        }
+
+        if(nextOut == rightNode){
+            reverseOrder(comp, maxNode, nextOut);
+            nextOut.setNextNode(nextIn);
+        } else if(nextOut == leftNode){
+            nextOut.setNextNode(nextIn);
+        }
+
+        return nextIn;
+    }
+
+    private void reverseOrder(Cluster cluster, Node finish, Node start){
+        if(cluster.getClusters().size() < 2) return;
+        Node cur = start;
+        Node next = start.nextNode();
+        start.setNextNode(null);
+        while (cur != finish){
+            Node tmp = next.nextNode();
+            next.setNextNode(cur);
+            cur = next;
+            next = tmp;
+        }
+    }
+
+    private Node getPrevNode(Cluster comp, Node node){
+        if(comp.getClusters().size() < 2) return null;
+        for(Node n : comp.getNodes()){
+            if(n.nextNode().equals(node)){
+                return n;
+            }
+        }
+        throw new UnsupportedOperationException();
+    }
+
+    private Pair<Node, Node> findMinNodesBClusters(Cluster n1, Cluster n2){
+        double min = Double.MAX_VALUE;
+        Pair<Node, Node> pair = null;
+        for(Node node1 : n1.getNodes()){
+            for(Node node2 : n2.getNodes()){
+                double cur = TownUtils.getDist(node1, node2);
+                if(cur < min){
+                    min = cur;
+                    pair = new Pair<>(node1, node2);
+                }
+            }
+        }
+        return pair;
+    }
+
+    private Pair<Node, Node> findMinNodesBClusters(Node node, Cluster n2){
+        double min = Double.MAX_VALUE;
+        Pair<Node, Node> pair = null;
+        for(Node node2 : n2.getNodes()){
+            double cur = TownUtils.getDist(node, node2);
+            if(cur < min){
+                min = cur;
+                pair = new Pair<>(node, node2);
+            }
+        }
+        return pair;
+    }
+
+    private void setOptPathCycle(Composite cluster){
+        int n = cluster.getSize();
+        List<Integer> order = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            order.add(i);
+        }
+
+        List<Integer> minOrder = new ArrayList<>(order);
+        double min = getDistCycle(minOrder, cluster);
+        do {
+            double cur = getDistCycle(order, cluster);
+            if(cur < min){
+                min = cur;
+                for(int i = 0; i < n; i++){
+                    minOrder.set(i, order.get(i));
+                }
+            }
+        } while (nextOrder(order));
+
+        setPathForInternalSol(minOrder, cluster);
+    }
+
+    private boolean nextOrder(List<Integer> curOrder){
+        int j = curOrder.size() - 2;
+        while (j != -1 && curOrder.get(j) >= curOrder.get(j + 1)) j--;
+        if(j == -1) return false;
+        int k = curOrder.size() - 1;
+        while (curOrder.get(j) >= curOrder.get(k)) k--;
+        int tmp = curOrder.get(j);
+        curOrder.set(j, curOrder.get(k));
+        curOrder.set(k, tmp);
+        int l = j + 1;
+        int r = curOrder.size() - 1;
+        while (l < r){
+            tmp = curOrder.get(l);
+            curOrder.set(l++, curOrder.get(r));
+            curOrder.set(r--, tmp);
+        }
+        return true;
+    }
+
+    private void setPathForInternalSol(List<Integer> orderClust, Composite clust){
+        if(orderClust.size() != clust.getSize()) throw new UnsupportedOperationException();
+        for(int i = 0; i < orderClust.size() - 1; i++){
+            int k1 = orderClust.get(i);
+            int k2 = orderClust.get(i + 1);
+            Node n1 = clust.getNodeByNum(k1);
+            Node n2 = clust.getNodeByNum(k2);
+            if(n1 != null && n2 != null){
+                n1.setNextNode(n2);
+            }
+        }
+        Node n1 = clust.getNodeByNum(orderClust.get(0));
+        Node n2 = clust.getNodeByNum(orderClust.get(orderClust.size() - 1));
+        if(n1 != null && n2 != null) {
+            n2.setNextNode(n1);
+        }
+    }
+
+    private double getDistCycle(List<Integer> numClust, Composite cluster){
+        if(numClust.size() != cluster.getSize()) throw new UnsupportedOperationException();
+        if(numClust.size() == 1) return 0d;
+        double sumDist = 0;
+        for(int i = 0; i < numClust.size() - 1; i++){
+            int k1 = numClust.get(i);
+            int k2 = numClust.get(i + 1);
+            Node n1 = cluster.getNodeByNum(k1);
+            Node n2 = cluster.getNodeByNum(k2);
+            if(n1 != null && n2 != null){
+                sumDist += TownUtils.getDist(n1, n2);
+            } else {
+                throw new UnsupportedOperationException();
+            }
+        }
+        if(numClust.size() == 2) return sumDist;
+        Node n1 = cluster.getNodeByNum(numClust.get(0));
+        Node n2 = cluster.getNodeByNum(numClust.get(numClust.size() - 1));
+        if(n1 != null && n2 != null){
+            sumDist += TownUtils.getDist(n1, n2);
+        } else {
+            throw new UnsupportedOperationException();
+        }
+        return sumDist;
     }
 
     private Cluster innerClustering(List<Town> l, int num, int level){
-        if(level >= k2) return null;
         List<Town> list = new ArrayList<>(l);
-        if(list.size() <= k3) {
+        if(list.size() <= k3 /*|| level >= k2*/) {
             Cluster composite = new Cluster(num);
             int i = 0;
             for(Town t : list){
@@ -50,7 +283,7 @@ public class BaseAlg {
         markedTown.add(maxPair.getKey());
         list.removeAll(markedTown);
 
-        int counter = 0;
+        int counter = 2;
         while (counter < k1) {
             //fixTown, toTown, dist
             Map<Pair<Town, Town>, Double> map = getMinDistToMarked(markedTown, list);
@@ -68,7 +301,9 @@ public class BaseAlg {
         int i = 0;
         for(List<Town> list1 : res){
             Composite res1 = innerClustering(list1, i++,level + 1);
-            clu.add((Node)res1);
+            if(res1 != null) {
+                clu.add((Node) res1);
+            }
         }
         return clu;
     }
--- src/com/traning/task5/Main.java
+++ src/com/traning/task5/Main.java
@@ -1,8 +1,13 @@
 package com.traning.task5;
 
+import com.traning.task5.parsers.Parser;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        TaskWrapper taskWrapper = new TaskWrapper();
+        Model m = Parser.parse(new File("D:/Box/traveling-salesman-problem/resources/2.txt"));
+        Model taskWrapper = new Model();
         taskWrapper.addTown(0,0, 0);
         taskWrapper.addTown(1,1, 1);
         taskWrapper.addTown(2,2,2);
@@ -10,8 +15,8 @@ public class Main {
         taskWrapper.addTown(3,0,4);
         taskWrapper.addTown(0,3,5);
         taskWrapper.addTown(5,5,6);
-        BaseAlg b = new BaseAlg(taskWrapper);
-        b.solve();
-        System.out.println();
+        BaseAlg b = new BaseAlg(m);
+        double d = b.solve();
+        System.out.println(d);
     }
 }
